a910008e169bd260fd5eb3907729c34e1ef223c4
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2020-10-10 23:25:02 -0500

    Add Addon.add_listener Add Addon.__or__ to merge addons

--- fubuki/modules/addon/addon.py
+++ fubuki/modules/addon/addon.py
@@ -1,3 +1,5 @@
+from types import MethodType
+
 from discord.ext import commands
 
 class Addon(commands.Cog):
@@ -26,3 +28,39 @@ class Addon(commands.Cog):
                 subcmd.cog = self # Update the subcmds
 
         return self.bot.get_command(command.name)
+
+    def add_listener(self, listener, name = None):
+        setattr(
+            self,
+            listener.__name__,
+            MethodType(
+                listener.__func__ if isinstance(listener, MethodType) else listener,
+                self)
+            ) # Bind the listener to the object as a method
+        self.__cog_listeners__.append((name or listener.__name__, listener.__name__)) # Add it to the list
+
+        for name, method_name in self.__cog_listeners__:
+            self.bot.remove_listener(getattr(self, method_name)) # Just in case I guess
+            self.bot.add_listener(getattr(self, method_name), name) # Register it as a listener
+
+    def _merge_addon(self, other):
+        '''
+        Handles merging 2 addons together.
+        Generally for internal use
+        '''
+        for _cmd in other.get_commands(): # Add all commands over
+            self.add_command(_cmd)
+        for name, method_name in other.__cog_listeners__: # Add all listeners over
+            self.add_listener(getattr(other, method_name), name)
+        self.bot.remove_cog(other.qualified_name) # Consume the other addon
+
+    def __or__(self, other):
+        '''
+        Uses the `|` operator to merge two addons together.
+        When merged, all commands and listeners from the second addon
+        will be added to the first addon, consuming the second addon
+        in the process.
+        '''
+        self._merge_addon(other)
+        return self
+        
