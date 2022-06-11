e9afd2787154946a2e1c86497e2e1fd6ed98429d
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2022-06-10 23:11:12 -0500

    refactor: auto-defer before sending

--- neo/classes/interaction.py
+++ neo/classes/interaction.py
@@ -18,23 +18,20 @@ class AutoEphemeralInteractionResponse(discord.InteractionResponse):
         return await super().defer(ephemeral=_ephemeral, thinking=thinking)
 
     async def send_message(self, *args, **kwargs) -> None:
+
         if (
             self._parent.type == discord.InteractionType.application_command
             and "ephemeral"
             not in kwargs  # kwargs take priority, so skip if it exists there
         ):
-            if self.is_done():
-                await self._parent.followup.send(
-                    *args,
-                    ephemeral=getattr(self._parent.namespace, "ephemeral", True),
-                    **kwargs
-                )
-                return
+            if not self.is_done():
+                await self.defer()
 
-            return await super().send_message(
+            await self._parent.followup.send(
                 *args,
                 ephemeral=getattr(self._parent.namespace, "ephemeral", True),
                 **kwargs
             )
+            return
 
         return await super().send_message(*args, **kwargs)
