048e36d332b322cde35b46c973081f35578311e3
sardonicism-04 <110789901+sardonicism-04@users.noreply.github.com>
2022-06-20 12:06:06 -0500

    refactor: remove unused classes from `app_commands.py`

--- neo/classes/app_commands.py
+++ neo/classes/app_commands.py
@@ -2,26 +2,24 @@
 # Copyright (C) 2022 sardonicism-04
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Optional, ParamSpec, TypeVar, Union, cast
+from typing import TYPE_CHECKING, Any, Optional, ParamSpec, TypeVar, cast
 
-import discord
-from discord.ext import commands
-from neo.classes.context import NeoContext
+from discord import AppCommandOptionType, Interaction, app_commands
+from discord.ext.commands import Cog
 
 if TYPE_CHECKING:
-    from neo import Neo
-    from discord.app_commands import Group, Namespace
     from discord.app_commands.commands import CommandCallback
+    from neo import Neo
 
 T = TypeVar("T")
 P = ParamSpec("P")
 
-GroupT = TypeVar("GroupT", bound=Union["Group", "commands.Cog"])
+GroupT = TypeVar("GroupT", bound=app_commands.Group | Cog)
 
 
 def get_ephemeral(
-    interaction: discord.Interaction,
-    namespace: Optional[discord.app_commands.Namespace | dict[str, Any]] = None,
+    interaction: Interaction,
+    namespace: Optional[app_commands.Namespace | dict[str, Any]] = None,
 ) -> bool:
     """Given an Interaction and a namespace, determines whether or not the output should be ephemeral"""
     if TYPE_CHECKING:
@@ -42,14 +40,14 @@ def get_ephemeral(
     return ephemeral
 
 
-class AutoEphemeralAppCommand(discord.app_commands.Command[GroupT, P, T]):
+class AutoEphemeralAppCommand(app_commands.Command[GroupT, P, T]):
     def __init__(
         self,
         *,
         name: str,
         description: str,
         callback: CommandCallback[GroupT, P, T],
-        parent: Optional[Group] = None,
+        parent: Optional[app_commands.Group] = None,
         guild_ids: Optional[list[int]] = None,
         nsfw: bool = False,
     ):
@@ -62,25 +60,25 @@ class AutoEphemeralAppCommand(discord.app_commands.Command[GroupT, P, T]):
             nsfw=nsfw,
         )
 
-        # Inject an `ephemeral` parameter to every app commmand
-        self._params["private"] = discord.app_commands.transformers.CommandParameter(
+        # Inject a `private` parameter to every app commmand
+        self._params["private"] = app_commands.transformers.CommandParameter(
             name="private",
             description="Whether or not to send the command result privately",
             required=False,
             default=None,
-            type=discord.AppCommandOptionType.boolean,
+            type=AppCommandOptionType.boolean,
         )
 
     async def _invoke_with_namespace(
         self,
-        interaction: discord.Interaction,
-        namespace: discord.app_commands.Namespace,
+        interaction: Interaction,
+        namespace: app_commands.Namespace,
     ) -> T:
         if not getattr(self.callback, "no_defer", False):
             await interaction.response.defer()
 
         if not await self._check_can_run(interaction):
-            raise discord.app_commands.CheckFailure(
+            raise app_commands.CheckFailure(
                 f"The check functions for command {self.name!r} failed."
             )
 
@@ -89,56 +87,3 @@ class AutoEphemeralAppCommand(discord.app_commands.Command[GroupT, P, T]):
 
         transformed_values.pop("private", None)
         return await self._do_call(interaction, transformed_values)
-
-
-class AutoEphemeralHybridAppCommand(commands.hybrid.HybridAppCommand):
-    def __init__(self, wrapped, app_command_name: Optional[str]) -> None:
-        super().__init__(wrapped)
-        self.name = app_command_name or self.name
-
-        # Inject an `ephemeral` parameter to every hybrid commmand
-        self._params["private"] = discord.app_commands.transformers.CommandParameter(
-            name="private",
-            description="Whether to send the command result privately",
-            required=False,
-            default=None,
-            type=discord.AppCommandOptionType.boolean,
-        )
-
-    async def _invoke_with_namespace(
-        self, interaction: discord.Interaction, namespace: Namespace
-    ) -> Any:
-        await interaction.response.defer()
-        return await super()._invoke_with_namespace(interaction, namespace)
-
-
-class AutoEphemeralHybridCommand(commands.HybridCommand):
-    def __init__(self, func, /, **kwargs):
-        app_command_name = kwargs.pop("app_command_name", None)
-        super().__init__(func, **kwargs)
-        self.app_command = (
-            AutoEphemeralHybridAppCommand(self, app_command_name)
-            if self.with_app_command
-            else None
-        )
-
-    async def _parse_arguments(self, ctx: NeoContext):
-        interaction = ctx.interaction
-
-        if interaction is None:
-            return await super()._parse_arguments(ctx)
-
-        elif self.app_command:
-            kwargs = await self.app_command._transform_arguments(
-                interaction, interaction.namespace
-            )
-            interaction.namespace.ephemeral = get_ephemeral(interaction, kwargs)  # type: ignore
-            kwargs.pop("private", None)
-            ctx.kwargs = kwargs
-
-    async def invoke(self, ctx: NeoContext, /) -> None:
-        if self.with_command and ctx.interaction is None:
-            return await super().invoke(ctx)
-
-        if self.with_app_command and self.app_command and ctx.interaction is not None:
-            return await super().invoke(ctx)
