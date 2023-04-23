d97160c9c8f744f871ff075c2534caf9514e11d5
sardonicism-04 <sardony01@gmail.com>
2023-04-23 00:00:06 -0400

    fix: halve search results to improve response time

--- neo/addons/utility.py
+++ neo/addons/utility.py
@@ -140,7 +140,7 @@ class Utility(neo.Addon):
     async def google_command_callback(
         self, interaction: discord.Interaction, query: str, image: bool = False
     ):
-        resp = await self.google.search(query, image=image, results=100)
+        resp = await self.google.search(query, image=image, results=50)
 
         embeds = [*map(result_to_embed, resp)]
         if not embeds:
