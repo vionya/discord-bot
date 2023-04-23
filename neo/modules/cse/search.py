3f70fcca7b108e600c1cba928f146e03e793a0d7
sardonicism-04 <sardony01@gmail.com>
2023-04-22 23:56:53 -0400

    feat: overhaul cse search
    - now implements pagination to fetch up to 100 results
    - keys are now cycled on a per-attempt basis
      - this helps account for when the keys become valid again

--- neo/modules/cse/search.py
+++ neo/modules/cse/search.py
@@ -6,7 +6,7 @@ from typing import Optional
 from aiohttp import ClientSession
 from yarl import URL
 
-from .objects import GoogleResponse
+from .objects import GoogleResponse, SearchResult
 
 SEARCH_BASE = URL("https://www.googleapis.com/customsearch/v1")
 GoogleError = type("GoogleError", (Exception,), {})
@@ -17,50 +17,89 @@ def _safe(_input):
 
 
 class Search:
-    __slots__ = ("key", "engine_id", "session")
+    __slots__ = ("keys", "engine_id", "session")
 
     def __init__(
         self,
         *,
-        key: str | list[str],
+        keys: list[str],
         engine_id: str,
         session: Optional[ClientSession] = None,
     ):
-        self.key = key
+        self.keys = keys
         self.engine_id = engine_id
         self.session = session or ClientSession()
 
-    async def _perform_search(self, query, *, safesearch=True, image=False):
-
-        key = self._get_key()
-        params = dict(
-            q=query, key=key, cx=self.engine_id, safe=_safe(safesearch)
-        )
-
-        if image is True:
+    async def search(
+        self,
+        query: str,
+        *,
+        safesearch: bool = True,
+        image: bool = False,
+        results: int = 10,
+    ) -> list[SearchResult]:
+        params = dict(q=query, cx=self.engine_id, safe=_safe(safesearch))
+        if image:
             params.update(searchType="image")
 
-        async with self.session.get(SEARCH_BASE.with_query(**params)) as resp:
+        resp = await self._paginate_requests(
+            SEARCH_BASE.with_query(**params), results=results
+        )
+        ret: list[SearchResult] = []
+        for data in resp:
+            ret.extend(data.results)
+        return ret
 
-            _data = await resp.json()
-            if isinstance((error := _data.get("error")), dict):
-                if (
-                    error["code"] == 429
-                    and error["status"] == "RESOURCE_EXHAUSTED"
-                ):
-                    if isinstance(self.key, list):
-                        self.key.remove(key)
-                        return await self._perform_search(
-                            query, safesearch=safesearch, image=image
-                        )  # Try to get a new key to use
+    async def _paginate_requests(self, url: URL, *, results: int = 10):
+        if results > 100:
+            raise ValueError(
+                "No more than 100 results may be requested at once"
+            )
+
+        # a copy of the key register to cycle through in case of too many reqs
+        available_keys = self.keys[:]
+        # randomly chosen key
+        key = choice(available_keys)
+        # index to start paginating at
+        pagination_index = 1
+        # all responses received thus far
+        resps: list[GoogleResponse] = []
+
+        while results > 0:
+            async with self.session.get(
+                url
+                % {
+                    "key": key,
+                    "start": pagination_index,
+                    "num": min(10, results),
+                }
+            ) as resp:
+                data = await resp.json()
+
+                if isinstance((error := data.get("error")), dict):
+                    # if we get an "out of requests" error, then check if we
+                    # still have more keys available
+                    if (
+                        error["code"] == 429
+                        and error["status"] == "RESOURCE_EXHAUSTED"
+                        and len(available_keys) > 0
+                    ):
+                        # if so, remove the dead key
+                        available_keys.remove(key)
+                        # replace it with a new one and try again
+                        key = choice(available_keys)
+                        continue
+                    # otherwise give up
                     raise GoogleError(f"Error with search ({error['code']})")
 
-        return GoogleResponse(_data)
+                data_obj = GoogleResponse(data)
+                resps.append(data_obj)
+                if not data_obj.next_page:
+                    # there are no results on upcoming pages, so don't waste
+                    # requests, and return with what we've got
+                    return resps
 
-    def search(self, *args, **kwargs):
-        return self._perform_search(*args, **kwargs)
+            results -= 10
+            pagination_index += 10
 
-    def _get_key(self):
-        if isinstance(self.key, list):
-            return choice(self.key)
-        return self.key
+        return resps
